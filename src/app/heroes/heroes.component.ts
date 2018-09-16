d596197c910dd11c6731f621eb80ae25b6af081d
Deepalakshmi Thiagarajan <deepa.name@gmail.com>
2018-09-16 14:59:56 -0700

    create hero service and get input data from the service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -14,15 +14,20 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
